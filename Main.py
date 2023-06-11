c2dc1780ede94502ec4fb0b82eadd2e13d23863d
sashaTribe <sasha555.tribe@gmail.com>
2023-06-11 20:06:14 +0100

    added find empty space

--- Main.py
+++ Main.py
@@ -15,6 +15,19 @@ for i in range(3):
 print(game_board)
 #can move up,down,left,right
 
+def create_child(current_board):
+    x,y = 
+    actions = [[x, y-1],[]]
+    
+def find_empty_space(current_board):
+    x = 0
+    y = 0
+    for i in range(3):
+        for j in range(3):
+            if current_board[i][j] == 0:
+                x = i
+                y = j
+    return x,y
 def a_star(start_node, end_node):
     open_set = set(start_node)
     closed_set = set()
@@ -25,4 +38,17 @@ def a_star(start_node, end_node):
     g[start_node] == 0
     parents[start_node] = start_node
 
-    
+    while len(open_set) > 0:
+        n = None
+
+        for v in open_set:
+            if n==None or g[v] + heuristic[v] < g[n] + heuristic[n]:
+                n = v
+        
+        if n == end_node or g[n] == None:
+            pass
+        else:
+            #for (m, action) in get_neighbours(n):
+
+
+
