dd97e47a38bb1c5d0f2aad4af02ea46b5d9ffe59
sashaTribe <sasha555.tribe@gmail.com>
2023-06-11 19:53:08 +0100

    initialised variables for A* algorithm

--- Main.py
+++ Main.py
@@ -15,4 +15,14 @@ for i in range(3):
 print(game_board)
 #can move up,down,left,right
 
-def a_star(game_board)
+def a_star(start_node, end_node):
+    open_set = set(start_node)
+    closed_set = set()
+    start_node = game_board
+    end_node = desired_goal
+    g = {} #distance from start node
+    parents = {} #adjacency map of all nodes for each parent
+    g[start_node] == 0
+    parents[start_node] = start_node
+
+    
