822b33d1034f9f926c9bd513e04b313602473c85
sashaTribe <sasha555.tribe@gmail.com>
2023-06-12 13:55:59 +0100

    convert design to an OO design

--- Main.py
+++ Main.py
@@ -18,29 +18,46 @@ open_set = set(game_board)
 closed_set = set()
 #can move up,down,left,right
 
-def generate_children(gameboard,x,y):
-    possible_vals = [[x, y-1],[x,y+1],[x-1,y],[x+1,y]]
-    children = []
-    for c in possible_vals:
-        child = create_child(game_board,x,y,c[0],c[1])
-        if child != None:
-            children.append(child)
-    return children
+class Node:
+    def __init__(self,board,level,fval):
+        self.board = board
+        self.level = level
+        self.fval = fval
 
-        
-def create_child(current_board,x1,y1, x2,y2):
-    temp_board = current_board
-    if temp_board[x2][y2] == 0:
-        temp_val = temp_board[x1][y1]
-        temp_board[x1][y1] = temp_board[x2][y2]
-        temp_board[x2][y2] = temp_val
-        return temp_board
-    else:
-        return None
+
+    def create_child(current_board,x1,y1, x2,y2):
+        temp_board = current_board
+        if temp_board[x2][y2] == 0:
+            temp_val = temp_board[x1][y1]
+            temp_board[x1][y1] = temp_board[x2][y2]
+            temp_board[x2][y2] = temp_val
+            new_child = Node(temp_board,current_board.level+1,0)
+            return new_child
+        else:
+            return None
+
+    def generate_children(self,gameboard,x,y):
+        possible_vals = [[x, y-1],[x,y+1],[x-1,y],[x+1,y]]
+        children = []
+        for c in possible_vals:
+            child = self.create_child(gameboard,x,y,c[0],c[1])
+            if child != None:
+                children.append(child)
+        return children
+
             
-
-
-
+    
+                
+    # function to calculate f(x) = g(x) + h(x)
+    def f(current_node, goal_node):
+        return h(current_node,goal_node) + current_node.level
+    def h(current_node, goal_node):
+        count = 0
+        for i in range(3):
+            for j in range(3):
+                if current_node[i][j] != goal_node[i][j] and current_node[i][j] != 0:
+                    count += 1
+        return count
 
 def update_gameboard(action, )
     
