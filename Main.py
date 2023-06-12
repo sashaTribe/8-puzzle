25cb69a3a0edd8ca5923a02fd9b10cddf0e302dd
sashaTribe <sasha555.tribe@gmail.com>
2023-06-12 13:28:02 +0100

    Create_child and generate_children implemented

--- Main.py
+++ Main.py
@@ -8,16 +8,41 @@ desired_goal = [[0,1,2],
 numbers = list(range(0,9))
 random.shuffle(numbers)
 
+closed_set = set()
 for i in range(3):
     for j in range(3):
         game_board[i][j] = numbers.pop()
         print(numbers)
 print(game_board)
+open_set = set(game_board)
+closed_set = set()
 #can move up,down,left,right
 
-def create_child(current_board):
-    x,y = 
-    actions = [[x, y-1],[]]
+def generate_children(gameboard,x,y):
+    possible_vals = [[x, y-1],[x,y+1],[x-1,y],[x+1,y]]
+    children = []
+    for c in possible_vals:
+        child = create_child(game_board,x,y,c[0],c[1])
+        if child != None:
+            children.append(child)
+    return children
+
+        
+def create_child(current_board,x1,y1, x2,y2):
+    temp_board = current_board
+    if temp_board[x2][y2] == 0:
+        temp_val = temp_board[x1][y1]
+        temp_board[x1][y1] = temp_board[x2][y2]
+        temp_board[x2][y2] = temp_val
+        return temp_board
+    else:
+        return None
+            
+
+
+
+
+def update_gameboard(action, )
     
 def find_empty_space(current_board):
     x = 0
@@ -28,9 +53,10 @@ def find_empty_space(current_board):
                 x = i
                 y = j
     return x,y
+
+
 def a_star(start_node, end_node):
-    open_set = set(start_node)
-    closed_set = set()
+    
     start_node = game_board
     end_node = desired_goal
     g = {} #distance from start node
