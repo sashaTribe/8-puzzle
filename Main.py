80ca1a0072970e8d9347cd94870e71ea8e770f49
sashaTribe <sasha555.tribe@gmail.com>
2023-06-12 19:54:17 +0100

    Update Main.py

--- Main.py
+++ Main.py
@@ -1,5 +1,5 @@
 import random
-
+import heapq
 
 
 desired_goal = [[0,1,2],
@@ -25,49 +25,58 @@ class Node:
         self.fval = fval
 
 
-    def create_child(self,x1,y1, x2,y2):
-        temp_board = self.board
+    def create_child(self,board,x1,y1, x2,y2):
+        temp_board = self.copy(board)
+        #print(board)
         k = range(0,3)
         if x2 in k and y2 in k:
-            temp_val = temp_board[x1][y1]
-            temp_board[x1][y1] = temp_board[x2][y2]
-            temp_board[x2][y2] = temp_val
+            temp_val = temp_board[x2][y2]
+            temp_board[x2][y2] = temp_board[x1][y1]
+            temp_board[x1][y1] = temp_val
             new_child = Node(temp_board,self.level+1,0)
             #print(new_child)
+            #print(temp_board)
             return new_child
         else:
             return None
 
-    def generate_children(self,x,y):
-        gameboard = self.board
+    def generate_children(self):
+        x,y = self.find_empty_space(0)
         possible_vals = [[x, y-1],[x,y+1],[x-1,y],[x+1,y]]
         children = []
         for c in possible_vals:
-            child = self.create_child(x,y,c[0],c[1])
+            child = self.create_child(self.board,x,y,c[0],c[1])
             if child != None:
                 
                 children.append(child)
         return children
     
-    def find_empty_space(self):
+    def find_empty_space(self,val):
         current_board = self.board
         x = 0
         y = 0
         for i in range(3):
             for j in range(3):
-                if current_board[i][j] == 0:
+                if current_board[i][j] == val:
                     x = i
                     y = j
         return x,y
 
-
+    def copy(self,board):
+        temp = []
+        for i in board:
+            t = []
+            for j in i:
+                t.append(j)
+            temp.append(t)
+        return temp  
             
 
 class Puzzle:
 
-    def __init__(self, open=[],close=[]):
-        self.open = open
-        self.close = close
+    def __init__(self):
+        self.open = []
+        self.close = set()
 
     # approximation heuristic
     def h(self, current_node, goal_node):
@@ -76,6 +85,7 @@ class Puzzle:
             for j in range(3):
                 if current_node[i][j] != goal_node[i][j] and current_node[i][j] != 0:
                     count += 1
+        print("Count: ", count)
         return count
 
 
@@ -93,17 +103,16 @@ class Puzzle:
         start_node = Node(start_board,0,0)
         end_node = desired_goal
         print(desired_goal)
-        '''
-        start_node.fval = self.f(start_node,end_node)
-
-        self.open.append(start_node)
         
+        start_node.fval = self.f(start_node,end_node)
+        self.open.append(start_node)
+
         print("The starting board: ")
         for i in start_node.board:
             for j in i:
                 print(j,end=" ")
             print("")
-        while True:
+        while open:
             
             current_status = self.open[0]
             print("Current Status: ")
@@ -114,17 +123,28 @@ class Puzzle:
                 print("")
             if self.h(current_status.board,end_node) == 0:
                 break
-            x,y = current_status.find_empty_space()
-            for i in current_status.generate_children(x,y):
+            #x,y = current_status.find_empty_space()
+
+            
+            children = current_status.generate_children()
+            for i in children:
                 i.fval = self.f(i,end_node)
                 self.open.append(i)
-            self.close.append(current_status)
+                print("Child Node: ", i.board)
+                
+            self.close.add(tuple(map(tuple, current_status.board)))
+            
             del self.open[0]
 
-            self.open.sort(key = lambda x:x.fval, reverse=False)
+            children.sort(key = lambda x:x.fval, reverse=False)
+            self.open[0] = children[0]
+            
+            #self.close.append(self.open[0])
+            
+            
             #print("Length of open array: ", len(self.open))
         print("Success!")
-'''
+
 puzzle = Puzzle()
 puzzle.a_star()
 
